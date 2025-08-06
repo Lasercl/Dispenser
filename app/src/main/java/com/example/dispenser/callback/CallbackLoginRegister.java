6991a4b99c9a2cc04e170c3a9c67304d3c3286b2
Lasercl <laserclauss2612@gmail.com>
2025-08-06 20:47:02 +0700

    add fitur login and register with  firebase auth

---
+++ app/src/main/java/com/example/dispenser/callback/CallbackLoginRegister.java
@@ -0,0 +1,5 @@
+package com.example.dispenser.callback;
+
+public interface CallbackLoginRegister<T> {
+    void onComplete(T result);
+}
--- app/src/main/java/com/example/dispenser/data/AuthRemoteDataSource.java
+++ app/src/main/java/com/example/dispenser/data/AuthRemoteDataSource.java
@@ -2,49 +2,49 @@ package com.example.dispenser.data;
 
 import android.util.Log;
 
+import com.example.dispenser.callback.CallbackLoginRegister;
 import com.example.dispenser.data.model.User;
-import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class AuthRemoteDataSource {
-    DatabaseReference rootDatabase= FirebaseDatabase.getInstance("https://dispenser-dc485-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
-    public Result<User> login(String username, String password) {
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-        try {
-            // TODO: handle loggedInUser authentication
-            User fakeUser =
-                    new User(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
-        }
+    DatabaseReference rootDatabase= FirebaseDatabase.getInstance("https://dispenser-dc485-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+    private String username;
+
+
+    public void login(String email, String password,CallbackLoginRegister<Result<User>> callback) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = task.getResult().getUser();
+                        callback.onComplete(new Result.Success<>(new User(user.getUid(), email)));
+                    } else {
+                        callback.onComplete(new Result.Error(task.getException()));
+                    }
+                });
     }
 
-    public Result<User> register(String username, String password) {
-
-        try {
-            // TODO: handle loggedInUser authentication
-            User user =
-                    new User(
-                            java.util.UUID.randomUUID().toString(),
-                            username);
-            user.setPassword(password);
-            rootDatabase.setValue(user)
-                    .addOnSuccessListener(aVoid -> Log.d("FIREBASE", "✅ Data berhasil disimpan"))
-                    .addOnFailureListener(e -> Log.e("FIREBASE", "❌ Gagal simpan data", e));
-
-            return new Result.Success<>(user);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
-        }
+    public void  register(String email, String password, CallbackLoginRegister<Result<User>> callback) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = task.getResult().getUser();
+                        callback.onComplete(new Result.Success<>(new User(user.getUid(), email)));
+                    } else {
+                        callback.onComplete(new Result.Error(task.getException()));
+                    }
+                });
     }
 
     public void logout() {
