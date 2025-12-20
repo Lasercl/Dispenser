c01c1eb958b4889940108e6d27f70b53fd5ac286
Lasercl <laserclauss2612@gmail.com>
2025-12-20 11:51:45 +0700

    upgrade home fragment

--- app/src/main/java/com/example/dispenser/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/dispenser/ui/home/HomeFragment.java
@@ -2,16 +2,23 @@ package com.example.dispenser.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dispenser.R;
+import com.example.dispenser.data.DispenserUtility;
+import com.example.dispenser.data.model.Dispenser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
 
@@ -28,6 +35,14 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private TextView deviceName;
+    private TextView nameLiquidTankA;
+    private TextView nameLiquidTankB;
+    private TextView volumeLiquidTankA;
+    private TextView volumeLiquidTankB;
+    private ImageView checklist;
+
+    private HomeViewModel mViewModel;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -62,6 +77,38 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        deviceName = view.findViewById(R.id.tvDeviceName);
+        volumeLiquidTankA = view.findViewById(R.id.tvLiquidPercentA);
+        volumeLiquidTankB=view.findViewById(R.id.tvLiquidPercentB);
+        nameLiquidTankA=view.findViewById(R.id.tvLabelA);
+        nameLiquidTankB=view.findViewById(R.id.tvLabelB);
+        checklist=view.findViewById(R.id.imageCheckHome);
+    }
+    private void updateDispenserUI(Dispenser dispenser) {
+        if (dispenser == null) return;
+        String dispenserStatus = DispenserUtility.getStatus(dispenser.getStatus());
+        // Checklist/Status Icon
+        if (dispenserStatus.equalsIgnoreCase("Available") || dispenser.getUserId() != null) {
+            checklist.setImageResource(R.drawable.check);
+        } else {
+            // Misalnya set ke icon lain jika sedang In Use oleh user lain
+            // checklist.setImageResource(R.drawable.uncheck);
+        }
+        nameLiquidTankA.setText("Liquid A: "+dispenser.getLiquidNameA());
+        nameLiquidTankB.setText("Liquid B: "+dispenser.getLiquidNameB());
+        volumeLiquidTankA.setText("Liquid: "+dispenser.getVolumeFilledA()+" %");
+        volumeLiquidTankB.setText("Liquid: "+dispenser.getVolumeFilledB()+" %");
+        deviceName.setText("Device Name: "+dispenser.getDeviceName());
+        // Asumsi Tank A");
+    }
+    private void startRealtime(String deviceId) {
+        mViewModel.listenDispenser(deviceId)
+                .observe(getViewLifecycleOwner(), this::updateDispenserUI);
     }
 
     @Override
@@ -106,4 +153,24 @@ public class HomeFragment extends Fragment {
 //        }
 //    }
 
+    private void showLastDispenser() {
+//        mViewModel.getLastDispenser().observe(getViewLifecycleOwner(), dispenser -> {
+//            if (dispenser != null){
+//                startRealtime(dispenser.getDeviceId());
+//            }
+//        });
+        String lastDispenserId = mViewModel.getDispenserLastId();
+        if (lastDispenserId != null) {
+            startRealtime(lastDispenserId);
+        }
+
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        // TODO: Use the ViewModel
+        showLastDispenser();
+
+    }
 }
