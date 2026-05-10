04176147d2f99e68468e693cf4c8d3660cbbad75
Lasercl <laserclauss2612@gmail.com>
2026-05-10 15:17:32 +0700

    make secure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.legacy.support.v4)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
